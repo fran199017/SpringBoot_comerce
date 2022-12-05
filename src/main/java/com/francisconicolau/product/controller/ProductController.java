63082fd3a96800e1cba7c7ddbf386812196d66cb
Francisco Nicolau <fran199017@gmail.com>
2022-12-05 12:24:08 +0100

    Update, and delete product

--- src/main/java/com/francisconicolau/product/controller/ProductController.java
+++ src/main/java/com/francisconicolau/product/controller/ProductController.java
@@ -1,14 +1,25 @@
 package com.francisconicolau.product.controller;
 
+import com.francisconicolau.prices.model.PriceSpecification;
+import com.francisconicolau.prices.model.Prices;
+import com.francisconicolau.prices.model.PricesSearch;
 import com.francisconicolau.product.model.Products;
+import com.francisconicolau.product.model.dto.ProductDTO;
 import com.francisconicolau.product.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @RequestMapping("/public")
@@ -38,5 +49,59 @@ public class ProductController {
         }
     }
 
+    @ApiOperation(value = "Get product by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Server Exception"),
+            @ApiResponse(code = 200, message = "OK", response = Products.class)
+    })
+    @GetMapping(value = "/products/{id}")
+    public ResponseEntity<Object> productsById(@PathVariable @ApiParam(value = "The product id", required = true) int id) {
+        try {
+            Products product = productService.getProductById(id);
+            if (product != null){
+                return new ResponseEntity<>(product, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    @ApiOperation(value = "Update product by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Server Exception"),
+            @ApiResponse(code = 200, message = "OK", response = Products.class)
+    })
+    @PutMapping(value = "/products/{id}")
+    public ResponseEntity<Object> updateProduct(@RequestBody ProductDTO productDTO,
+                                                @PathVariable @ApiParam(value = "The product id", required = true) int id) {
+        try {
+            Products updated = productService.updateProduct(id , productDTO);
+            if (updated != null){
+                return new ResponseEntity<>(updated, HttpStatus.OK);
+
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @ApiOperation(value = "Update product by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Server Exception"),
+            @ApiResponse(code = 200, message = "OK", response = Products.class)
+    })
+    @DeleteMapping(value = "/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable @ApiParam(value = "The product id", required = true) int id) {
+        try {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/francisconicolau/product/service/ProductService.java
+++ src/main/java/com/francisconicolau/product/service/ProductService.java
@@ -3,13 +3,11 @@ package com.francisconicolau.product.service;
 import com.francisconicolau.prices.model.Prices;
 import com.francisconicolau.prices.repository.PricesRepository;
 import com.francisconicolau.product.model.Products;
+import com.francisconicolau.product.model.dto.ProductDTO;
 import com.francisconicolau.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @Service("productService")
@@ -49,5 +47,55 @@ public class ProductService {
             }
         }
         return 0;
+    }
+
+    public Products getProductById(int id) {
+        Optional<Products> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()){
+            return optionalProduct.get();
+        }
+        return null;
+    }
+
+    public Products updateProduct(int id, ProductDTO productDTO) {
+        Products product = getProductById(id);
+
+        if (product != null){
+            String name = productDTO.getName();
+            String description = productDTO.getDescription();
+            float price = productDTO.getPrice();
+            String path = productDTO.getImagePath();
+
+            if (name != null){
+                product.setName(productDTO.getName());
+            }
+
+            if (description != null){
+                product.setDescription(productDTO.getDescription());
+            }
+
+            if (price > 0){
+                product.setPrice(productDTO.getPrice());
+            }
+
+            if (path != null){
+                product.setImagePath(productDTO.getImagePath());
+            }
+
+            Products saved = productRepository.save(product);
+            if (saved !=null){
+                return saved;
+            }
+        }
+
+        return null;
+    }
+
+    public void deleteProduct(int id) {
+        Products product = getProductById(id);
+        if (product != null){
+            productRepository.delete(product);
+        }
+
     }
 }
